28774a4b5f3273e1c4b9655526f097aaaf4a9068
Alvaro Mena <alvaro16a@gmail.com>
2022-02-11 21:28:45 -0500

    Se creo la clase ball y se dibujo la pelota en pantalla

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
     self.Board.prototype = {
         get elements() {             //este metodo retorna las barras y pelotas dentro del tablero
             var elements = this.bars;
-            //elements.push(this.ball);
+            elements.push(this.ball);
             return elements;
         }
     }
@@ -51,12 +51,30 @@
             case "rectangle":
                 ctx.fillRect(element.x, element.y, element.width, element.height);
                 break;
-
+            
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x, element.y, element.radius, 0, 7);
+                ctx.fill();
+                ctx.closePath();
+                break;
         }
     }
 })();
 
-//(function(){})();
+(function(){
+    self.ball = function(x,y,radius,board){
+        this.x = x;
+        this.y = y;
+        this.radius= radius;
+        this.speed_y = 0;
+        this.speed_x = 5;
+        this.board = board;
+
+        board.ball = this;
+        this.kind = "circle";
+    }
+})();
 
 (function () {
     self.Bar = function (x, y, width, height, board) {
@@ -92,6 +110,7 @@
 var board = new Board(800, 450);
 var bar_1 = new Bar(20, 100, 40, 100, board);
 var bar_2 = new Bar(740, 100, 40, 100, board);
+var ball = new ball(400,225,15,board);
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas, board);
 
